f49af39a0541e68f242b99bb2e76f36916e21393
SaltyFive <xxccyy_aa@163.com>
2025-09-28 13:42:20 +0800

    文章渲染界面的返回功能，以及.md存放（

--- app/pages/[...slug].vue
+++ app/pages/[...slug].vue
@@ -18,10 +18,18 @@ useSeoMeta(page.value?.seo || {})
 
 <template>
   <NuxtLayout :name="'article'" class="bg-white dark:bg-gray-800 ring-1 ring-gray-200 dark:ring-gray-700">
-    <ContentRenderer
+    <div v-if="!page">
+      未找到
+    </div>
+    <div v-else>
+      <ContentRenderer
       v-if="page"
       :value="page"
       class="prose dark:prose-invert prose-pre:bg-gray-100 dark:prose-pre:bg-gray-900"
     />
+    </div>
+    <div class="flex justify-center">
+      <Button @click="navigateTo('/')" class="bg-blue-100 text-gray-500 border border-blue-300 px-4 py-3 rounded">返回主页</Button>
+    </div> 
   </NuxtLayout>
 </template>
