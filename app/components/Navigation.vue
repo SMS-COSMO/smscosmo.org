5c2e6fd3e2634178ed90864cb5d18be6daac5e11
hycqwq <i@hycqwq.top>
2026-04-01 22:02:01 +0800

    feat: 新增导航栏隐藏功能

--- app/components/Navigation.vue
+++ app/components/Navigation.vue
@@ -1,46 +1,68 @@
 <template>
-  <div class="flex fixed top-0 z-50 w-full">
-    <div :class="navClass">
-      <div class="w-16 h-16 p-2.5 -mr-1">
-        <NuxtImg src="/cosmo-logo.svg" />
-      </div>
-      <div class="mt-auto mb-auto">
-        <span class="text-blue-400 font-[700]">
-          COSMO
-        </span>
-        <span class="text-zinc-6 font-[600] hidden md:inline-block">
-          智慧校园平台运研中心
-        </span>
-      </div>
-      <div class="flex ml-auto align-middle font-[600]">
-        <div v-for="item in NavItems" :key="item.path" class="mt-auto mb-auto mr-3 hover:text-gray-400 duration-300">
-          <NuxtLink :to="item.path" :title="item.description">
-            {{ item.title }}
-          </NuxtLink>
+  <AnimatePresence>
+    <motion.div
+      v-if="isContainerShown"
+      class="flex fixed top-0 z-50 w-full duration-500"
+      :initial="{
+        y: '-150%',
+      }"
+      :animate="{
+        y: 0,
+      }"
+      :exit="{
+        y: '-250%',
+      }"
+    >
+      <div :class="navClass">
+        <div class="w-16 h-16 p-2.5 -mr-1">
+          <NuxtImg src="/cosmo-logo.svg" />
+        </div>
+        <div class="mt-auto mb-auto">
+          <span class="text-blue-400 font-[700]">
+            COSMO
+          </span>
+          <span class="text-zinc-6 font-[600] hidden md:inline-block">
+            智慧校园平台运研中心
+          </span>
+        </div>
+        <div class="flex ml-auto align-middle font-[600]">
+          <div v-for="item in NavItems" :key="item.path" class="mt-auto mb-auto mr-3 hover:text-gray-400 duration-300">
+            <NuxtLink :to="item.path" :title="item.description">
+              {{ item.title }}
+            </NuxtLink>
+          </div>
         </div>
       </div>
-    </div>
-  </div>
+    </motion.div>
+  </AnimatePresence>
   <div class="h-28 w-full">
     <div ref="topItem" class="absolute top-0 h-10 w-full -z-50" />
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { AnimatePresence, motion } from 'motion-v';
+import { computed, ref } from 'vue';
+
+const props = defineProps({
+  hidden: Boolean,
+});
 
 const topItem = ref(null);
 
-const tallClass = 'flex m-5 p-0.5 w-full bg-blue-800/80 backdrop-blur-sm shadow-2xl rounded-md duration-500';
-const shortClass = 'flex p-0.5 w-full bg-black/50 backdrop-blur-sm shadow-2xl duration-500';
-const navClass = ref(tallClass);
+const isContainerShown = computed(() =>
+  !props.hidden);
+
+const tallNavClass = 'flex m-5 p-0.5 w-full bg-blue-800/70 backdrop-blur-sm shadow-2xl rounded-md duration-500 ease-in-out';
+const shortNavClass = 'flex p-0.5 w-full bg-black/50 backdrop-blur-sm shadow-2xl duration-500 ease-in-out';
+const navClass = ref(tallNavClass);
 let navState = 'tall';
 function toShort(): void {
-  navClass.value = shortClass;
+  navClass.value = shortNavClass;
   navState = 'short';
 }
 function toTall(): void {
-  navClass.value = tallClass;
+  navClass.value = tallNavClass;
   navState = 'tall';
 }
 // eslint-disable-next-line unused-imports/no-unused-vars
@@ -51,6 +73,7 @@ function swapNav(): void {
     toTall();
   }
 }
+
 onMounted(() => {
   const observer = new IntersectionObserver((entries) => {
     const entry = entries[0];
