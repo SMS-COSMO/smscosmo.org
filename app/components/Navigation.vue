7d0d604a9b8254aa2c5d9adfb2ba0424467f11e1
hycqwq <i@hycqwq.top>
2026-04-12 00:50:32 +0800

    fix: 优化nav动画逻辑 防止短nav运动过度 防止页面加载时播放nav动画

--- app/components/Navigation.vue
+++ app/components/Navigation.vue
@@ -1,4 +1,4 @@
-﻿<template>
+<template>
   <div
     class="fixed top-0 left-0 w-full z-50"
   >
@@ -9,7 +9,7 @@
         :initial="{ opacity: 0, y: '-150%' }"
         :animate="navStyle"
         :exit="{ opacity: 0, y: '-150%' }"
-        :transition="{ duration: 0.3, type: 'spring', stiffness: 80 }"
+        :transition="navTransition"
       >
         <div class="w-16 h-16 p-2.5 -mr-1">
           <NuxtImg src="/cosmo-logo.svg" />
@@ -34,11 +34,12 @@
       </motion.div>
     </AnimatePresence>
   </div>
-  <div class="h-28 w-full" />
+  <div class="h-28 w-full" /> <!-- 防止nav挡住下面的元素 -->
 </template>
 
 <script setup lang="ts">
-import { AnimatePresence, motion, optimizedAppearDataAttribute, useMotionValueEvent, useScroll } from 'motion-v';
+import type { $Transition } from 'motion-v'; // 用来规定类型，防止类型检查报错
+import { AnimatePresence, motion, useMotionValueEvent, useScroll } from 'motion-v';
 import { computed, ref } from 'vue';
 
 defineProps<{ hidden: boolean }>();
@@ -52,11 +53,12 @@ const NavItems = ref([
 // 滚动阈值（像素），超过此值切换为“短”样式
 const SCROLL_THRESHOLD = 100;
 const onTop = ref(false);
+const animationEnabled = ref(false);
 
 const { scrollY } = useScroll();
 useMotionValueEvent(scrollY, 'change', (latest) => {
   onTop.value = latest > SCROLL_THRESHOLD;
-  console.log('onTop:', onTop.value);
+  // console.log('onTop:', onTop.value);
 });
 
 const navStyle = computed(() => {
@@ -73,7 +75,7 @@ const navStyle = computed(() => {
     };
   } else {
     return {
-      y: 10,
+      y: '2vh',
       opacity: 1,
       backgroundColor: 'rgba(30, 64, 175, 0.8)',
       backdropFilter: 'blur(4px)',
@@ -83,5 +85,28 @@ const navStyle = computed(() => {
       x: '2.5%', // 这里是因为宽度变成了95%，所以左右各有2.5%的空白，所以往右边移动了2.5%
     };
   }
+});
+const navTransition = computed((): $Transition => {
+  if (!animationEnabled.value) {
+    return { duration: 0 }; // 为了防止页面初次加载时播放动画（页面加载完成的瞬间nav应处于就位状态），禁用动画效果
+  }
+  if (onTop.value) {
+    return {
+      duration: 0.5,
+      type: 'tween', // 如果是贴顶的形态，在出现时应该直接就位，而不是在竖直方向有多的移动（即，防止“短”样式的nav顶端低于可视范围顶端）
+      ease: 'easeOut',
+    };
+  } else {
+    return {
+      duration: 0.5,
+      type: 'spring',
+      stiffness: 70,
+    };
+  }
+});
+onMounted(() => {
+  setTimeout(() => {
+    animationEnabled.value = true;
+  }, 100); // 挂载之后0.1s再启用nav动画，参见navTransition中注释
 });
 </script>
