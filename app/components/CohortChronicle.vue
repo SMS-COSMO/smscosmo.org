51a553518f614e3664614773b165a856cc76b2f9
Haley <2549047487@qq.com>
2026-05-30 18:32:14 +0800

    feat: 完成 CohortChronicle 组件

---
+++ app/components/CohortChronicle.vue
@@ -0,0 +1,30 @@
+<template>
+  <motion.div
+    v-for="m in msg"
+    :key="m.cohort"
+    class="w-fullh-10 rounded bg-blue-500 p-4"
+    :initial="{ opacity: 0, y: 20 }"
+    :animate="{ opacity: 1, y: 0 }"
+    :while-in-view="{ opacity: 0.8, y: 20 }"
+    :transition="{ duration: 0.5 }"
+  >
+    <h3 class="text-xl font-bold">
+      第 {{ m.cohort }} 届
+    </h3>
+    <component :is="m.markdown" />
+
+    <!-- <li v-for="q in quotes" :key="q.id">
+    {{ q.quote }}
+  </li> -->
+  </motion.div>
+</template>
+
+<script setup lang="ts">
+interface CohortItem {
+  cohort: number;
+  markdown: string | any;
+}
+defineProps<{
+  msg: CohortItem[];
+}>();
+</script>
